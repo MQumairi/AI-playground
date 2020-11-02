bfdcdf5ba57699f34948e8487233d3f6b6d041b6
Mohammed Alqumairi <moh.alqumairi@gmail.com>
2020-11-02 00:53:39 +0000

    commit

--- LinearRegression.py
+++ LinearRegression.py
@@ -24,19 +24,18 @@ def lin_reg(x, theta0, theta1):
 
 plt.scatter(x, y)
 
+# parameters
 theta0 = 0
 theta1 = 0
-theta0_arr = []
-theta1_arr = []
+
+# Hyper parameters
 m = len(y)
 a = 0.01
-for i in range(len(y)):
-    theta0 -= a * ((sum(lin_reg(x, theta0, theta1) - y)) / m)
-    theta1 -= a * ((sum(lin_reg(x, theta0, theta1) - y) * x[i]) / m)
-    theta0_arr.append(theta0)
-    theta1_arr.append(theta1)
-    # To plot a line for each iteration, uncomment below
-    # plt.plot(x, lin_reg(x, theta0, theta1))
+
+for i in range(10):
+    gradient = (lin_reg(x, theta0, theta1) - y).transpose().dot(x) / len(x)
+    theta0 -= a * gradient
+    theta1 -= a * gradient
 
 plt.plot(x, lin_reg(x, theta0, theta1))
 # %%
